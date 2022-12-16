0215be2ea4f48ec6a26d1947a12451acb32c507a
Александр <alexakr1993@gmail.com>
2022-12-16 16:36:55 +0400

    First commit

--- src/Cars.java
+++ src/Cars.java
@@ -7,12 +7,28 @@ public class Cars {
     public String country;
 
     Cars(String brand, String model, double engineVolume, String color, int year, String country) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
+        if (model == null) {
+            this.model = "default";
+        } else this.model = model;
+        if (brand == null) {
+            this.brand = "default";
+        } else this.brand = brand;
+        if (country == null) {
+            this.country = "default";
+        } else this.country = country;
+
+        if (engineVolume <= 0) {
+           this.engineVolume = 1.5;
+        } else this.engineVolume = engineVolume;
+
+        if (color == null) {
+            this.color = "белого";
+        } else this.color = color;
+
+        if (year <= 0) {
+            this.year = 2000;
+        } else this.year = year;
+
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        Cars granta = new Cars("Lada","Granta",1.7,"России",2015,"России");
+        Cars granta = new Cars(null,"Granta",1.7,null,2015,"России");
         System.out.println(granta.brand + " " + granta.model + ", " + granta.year + " год выпуска, сборка в " + granta.country + ", " + granta.color +
                 " цвета, объем двигателя — " + granta.engineVolume + " л, ");
         System.out.println();
 
-        Cars audi = new Cars("Audi", "A8 50 L TDI quattro",3.0,"Германии",2020,"Германии");
+        Cars audi = new Cars("Audi", "A8 50 L TDI quattro",0,"Германии",2020,"Германии");
         System.out.println(audi.brand + " " + audi.model + ", " + audi.year + " год выпуска, сборка в " + audi.country + ", " + audi.color +
                 " цвета, объем двигателя — " + audi.engineVolume + " л.");
         System.out.println();
 
-        Cars bmw = new Cars("BMW","Z8",3.0,"черный",2021,"Германии");
+        Cars bmw = new Cars("BMW","Z8",3.0,"черного",2021,"Германии");
         System.out.println(bmw.brand + " " + bmw.model + ", " + bmw.year + " год выпуска, сборка в " + bmw.country + ", " + bmw.color +
                 " цвета, объем двигателя — " + bmw.engineVolume + " л.");
         System.out.println();
 
 
-        Cars kia = new Cars("Kia","Sportage 4-го поколения",2.4,"красный",2018, "Южной Корее");
+        Cars kia = new Cars("Kia","Sportage 4-го поколения",2.4,"красного",-2018, "Южной Корее");
         System.out.println(kia.brand + " " + kia.model + ", " + kia.year + " год выпуска, сборка в " + kia.country + ", " + kia.color +
                 " цвета, объем двигателя — " + kia.engineVolume + " л.");
         System.out.println();
 
-        Cars hunday = new Cars("Hyundai","Avante",1.6,"оранжевый",2018,"Южной Корее");
+        Cars hunday = new Cars("Hyundai",null,1.6,null,2018,"Южной Корее");
         System.out.println(hunday.brand + " " + hunday.model + ", " + hunday.year + " год выпуска, сборка в " + hunday.country + ", " + hunday.color +
                 " цвета, объем двигателя — " + hunday.engineVolume + " л.");
         System.out.println();
