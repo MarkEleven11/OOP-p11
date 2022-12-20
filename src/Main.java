d0d9f652b2e31610e7615cbbd771c220b3d01bd7
Александр <alexakr1993@gmail.com>
2022-12-20 15:12:44 +0400

    Done!

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         Cars bmw = new Cars("BMW","Z8",3.0,"черного",2021,"Германии","Автомат", "Родстер",
                 "А011КР", 3, true, null);
+
         System.out.println(bmw.getBrand() + " " + bmw.getModel() + ", " + bmw.getEngineVolume() + " год выпуска, сборка в " + bmw.getCountry() + ", " +
                 bmw.getColor() +  " цвета, объем двигателя — " + bmw.getEngineVolume() + " л. Коробка " + bmw.getTransmission() + ", " +
                 "тип кузова " + bmw.getCarStyle() + ". " + "Регистрационный номер " + bmw.getRegNumber() + ", количество мест для сидения - " + bmw.getSeatsNumber() +
